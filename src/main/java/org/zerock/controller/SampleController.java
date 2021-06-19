6ac7ce38318f4379c9cd53b52256cef88ee863b4
tpdn333 <tpdn333@gmail.com>
2021-06-19 13:36:53 +0900

    sampleController

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,107 @@
+package org.zerock.controller;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.TodoDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("*sample/*")
+@Log4j
+public class SampleController {
+
+	/*
+	 * @InitBinder public void InitBinder(WebDataBinder binder) { SimpleDateFormat
+	 * dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	 * binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat,
+	 * false)); }
+	 */
+
+	@RequestMapping("")
+	public void basic() {
+		log.info("basic...........");
+	}
+
+	@RequestMapping(value = "/basic", method = { RequestMethod.GET, RequestMethod.POST })
+	public void basicGet() {
+		log.info("basic get..........");
+	}
+
+	@GetMapping("/basicOnlyGet")
+	public void basicGet2() {
+		log.info("basic get only get");
+	}
+
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info("" + dto);
+
+		return "ex01";
+	}
+
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name : " + name);
+		log.info("age : " + age);
+
+		return "ex02";
+	}
+
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids : " + ids);
+
+		return "ex02List";
+	}
+
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		log.info("array ids : " + Arrays.toString(ids));
+
+		return "ex02Array";
+	}
+
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo : " + todo);
+		return "ex03";
+	}
+
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto : " + dto);
+		log.info("page : " + page);
+		
+		return "/sample/ex04";
+	}
+
+	@GetMapping("exUpload")
+	public void exUpload() {
+		log.info("/exUpload..........");
+	}
+
+	@PostMapping("exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			log.info("----------------------");
+			log.info("name : " + file.getOriginalFilename());
+			log.info("size : " + file.getSize());
+		});
+	}
+}
