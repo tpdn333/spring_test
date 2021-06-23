6ea0fc77decee6994aa40716aab7d56b23b7e87b
tpdn333 <tpdn333@gmail.com>
2021-06-23 17:54:27 +0900

    board Test

---
+++ src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -0,0 +1,98 @@
+package org.zerock.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {org.zerock.config.RootConfig.class,
+								org.zerock.config.ServletConfig.class})
+@Log4j
+// Test for Controller
+@WebAppConfiguration
+public class BoardControllerTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private WebApplicationContext ctx;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception {
+		log.info(
+				mockMvc.perform(get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		String resultPage = mockMvc.perform(post("/board/register")
+				.param("title", "테스트 새글 제목")
+				.param("content", "테스트 새글 제목")
+				.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+		log.info(mockMvc.perform(get("/board/get")
+				.param("bno", "4")
+				).andReturn().getModelAndView().getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = mockMvc.perform(post("/board/modify")
+				.param("bno", "1")
+				.param("title", "수정된 테스트 새글 제목")
+				.param("content", "수정된 테스트 새글 내용")
+				.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		String resultPage = mockMvc.perform(post("/board/remove")
+				.param("bno", "7")).andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+				
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
