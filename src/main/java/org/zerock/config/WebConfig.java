a251fbb21cace0f1409835500eb7271799330034
tpdn333 <tpdn333@gmail.com>
2021-06-23 17:53:35 +0900

    WebConfig

--- src/main/java/org/zerock/config/WebConfig.java
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -1,32 +1,41 @@
 package org.zerock.config;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 //web.xml 설정파일 대신 
-public class WebConfig extends 
-		AbstractAnnotationConfigDispatcherServletInitializer{
-	
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {RootConfig.class};
+		return new Class[] { RootConfig.class };
 	}
-	
+
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class[] { ServletConfig.class };
 	}
-	
+
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
-	
+
 	@Override
 	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
 		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter cEF = new CharacterEncodingFilter();
+		cEF.setEncoding("UTF-8");
+		cEF.setForceEncoding(true);
+		return new Filter[] { cEF };
 	}
 
 }
