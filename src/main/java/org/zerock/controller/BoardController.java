d6d209197bede2d9a739b2d383b554a243336afa
tpdn333 <tpdn333@gmail.com>
2021-06-24 23:42:53 +0900

    controller

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.PageDTO;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -21,9 +23,10 @@ public class BoardController {
 	private BoardService service;
 
 	@GetMapping("list")
-	public void list(Model model) {
+	public void list(Criteria cri, Model model) {
 		log.info("list method get방식으로 실행!!");
-		model.addAttribute("list", service.getList());
+		model.addAttribute("list", service.getList(cri));
+		model.addAttribute("pageMaker", new PageDTO(cri, 123));
 	}
 
 	@PostMapping("register")
