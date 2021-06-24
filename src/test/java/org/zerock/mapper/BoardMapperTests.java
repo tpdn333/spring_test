8369870d223520cc796b52a4da82b90e9c75647a
tpdn333 <tpdn333@gmail.com>
2021-06-24 23:43:11 +0900

    test

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,11 +1,14 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -70,6 +73,14 @@ public class BoardMapperTests {
 		
 		int cnt = mapper.update(board);
 		log.info("UPDATE COUNT : " + cnt);
+	}
+	
+	@Test
+	public void testPaging() {
+		Criteria cri = new Criteria();
 		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board));
 	}
 }
