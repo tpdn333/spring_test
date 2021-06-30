74514f8dd522923f38ce21cc213809acfb8dc8b6
tpdn333 <tpdn333@gmail.com>
2021-06-30 16:24:54 +0900

    replyController

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -1,13 +1,20 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -26,7 +33,7 @@ public class ReplyController {
 			consumes = "application/json",
 			produces = { MediaType.TEXT_PLAIN_VALUE })
 	
-	public ResponseEntity<String> create(@RequestParam ReplyVO vo) {
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo) {
 		log.info("ReplyVO : " + vo);
 		
 		int insertCount = service.register(vo);
@@ -36,7 +43,51 @@ public class ReplyController {
 		// 삼항 연산자
 		return insertCount == 1 ? new ResponseEntity<>("Success", HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-				
+	}
 	
+	@GetMapping(value = "pages/{bno}/{page}",
+				produces = { MediaType.APPLICATION_XML_VALUE,
+							MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable int page,
+			@PathVariable Long bno) {
+		log.info("getList method 실행");
+		Criteria cri = new Criteria(page, 10);
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "{rno}",
+				produces = { MediaType.APPLICATION_XML_VALUE,
+							MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<ReplyVO> get(@PathVariable Long rno) {
+		log.info("get : " + rno);
+		
+		return new ResponseEntity<ReplyVO>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "{rno}", 
+				produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> remove(@PathVariable Long rno) {
+		log.info("remove : "+ rno);
+		
+		return service.remove(rno) == 1 ? new ResponseEntity<>("remove success", HttpStatus.OK)
+										: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(value = "{rno}", method = { RequestMethod.PUT, RequestMethod.PATCH },
+					consumes = "application/json",
+					produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo,
+										@PathVariable Long rno){
+		vo.setRno(rno);
+		
+		log.info("rno : " + rno);
+		log.info("modify : " + rno);
+		
+		return service.modify(vo) == 1
+				? new ResponseEntity<>("modify success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
