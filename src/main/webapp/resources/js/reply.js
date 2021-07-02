8922328e4b4af2cc7f8e8ac2e7033130e998ed76
tpdn333 <tpdn333@gmail.com>
2021-07-02 16:56:07 +0900

    java script

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -1,26 +1,98 @@
 console.log("Reply Module.........");
 
-var replyService = (function() {
-	
+var replyService = (function () {
+
 	function add(reply, callback, error) {
 		console.log("add reply........");
-		
+
 		$.ajax({
-			type : "post",
-			url : "/replies/new",
-			data : JSON.stringify(reply),
-			contentType : "application/json",
-			success : function(result, status, xhr) {
-				if(callback) {
+			type: "post",
+			url: appRoot + "/replies/new",
+			data: JSON.stringify(reply),
+			contentType: "application/json",
+			success: function (result, status, xhr) {
+				if (callback) {
 					callback(result);
 				}
 			},
-			error : function(xhr, status, er) {
-				if(error) {
+			error: function (xhr, status, er) {
+				if (error) {
 					error(er);
 				}
 			}
 		});
 	}
-	return { add : add };
+
+	function getList(param, callback, error) {
+		var bno = param.bno;
+		var page = param.page || 1;
+
+		$.getJSON(appRoot + "/replies/pages/" + bno + "/" + page + ".json",
+			function (data) {
+				if (callback) {
+					callback(data);
+				}
+			}).fail(function (xhr, status, err) {
+				if (error) {
+					error();
+				}
+			});
+	}
+
+	function remove(rno, callback, error) {
+		$.ajax({
+			type: "delete",
+			url: appRoot + "/replies/" + rno,
+			success: function (deleteResult, status, xhr) {
+				if (callback) {
+					callback(deleteResult);
+				}
+			},
+			error: function (xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+
+	function update(reply, callback, error) {
+		console.log("RNO :" + reply.rno);
+
+		$.ajax({
+			type: "put",
+			url: appRoot + "/replies/" + reply.rno,
+			data: JSON.stringify(reply),
+			contentType: "application/json; charset=utf-8",
+			success: function (result, status, xhr) {
+				if (callback) {
+					callback(result);
+				}
+			},
+			error: function (xhr, status, er) {
+				if (error) {
+					error(er);
+				}
+			}
+		});
+	}
+
+	function get(rno, callback, error) {
+		$.get(appRoot + "/replies/" + rno + ".json", function (result) {
+			if (callback) {
+				callback(result);
+			}
+		}).fail(function (xhr, status, err) {
+			if (error) {
+				error();
+			}
+		});
+	}
+	return {
+		add: add,
+		getList: getList,
+		remove: remove,
+		update: update,
+		get: get
+	};
 })();
