b28c3fefe30dac6917637c1b7831e71e40b00a9f
tpdn333 <tpdn333@gmail.com>
2021-07-08 18:27:19 +0900

    js

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -27,31 +27,32 @@ var replyService = (function () {
 		var bno = param.bno;
 		var page = param.page || 1;
 
-		// $.getJSON(appRoot + "/replies/pages/" + bno + "/" + page + ".json",
-		// 	function (data) {
+		$.getJSON(appRoot + "/replies/pages/" + bno + "/" + page + ".json",
+			function (data) {
+				if (callback) {
+					// callback(data); //댓글 목록만 가져오는 경우
+					callback(data.replyCnt, data.list);
+					console.log("댓글 목록 가져옴");
+				}
+			}).fail(function (xhr, status, err) {
+				if (error) {
+					error();
+				}
+			});
+		// $.ajax({
+		// 	type: "get",
+		// 	url: appRoot + "/replies/pages/" + bno + "/" + page + ".json",
+		// 	success: function (data) {
 		// 		if (callback) {
-		// 			// callback(data); //댓글 목록만 가져오는 경우
 		// 			callback(data.replyCnt, data.list);
 		// 		}
-		// 	}).fail(function (xhr, status, err) {
+		// 	},
+		// 	error: function (xhr, status, er) {
 		// 		if (error) {
-		// 			error();
+		// 			error(er);
 		// 		}
-		// 	});
-		$.ajax({
-			type: "get",
-			url: appRoot + "/replies/pages/" + bno + "/" + page + ".json",
-			success: function (data) {
-				if (callback) {
-					callback(data.replyCnt, data.list);
-				}
-			},
-			error: function (xhr, status, er) {
-				if (error) {
-					error(er);
-				}
-			}
-		});
+		// 	}
+		// });
 	}
 
 	function remove(rno, callback, error) {
